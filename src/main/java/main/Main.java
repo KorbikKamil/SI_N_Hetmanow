ab8b3dfd25d25ce18edf10e7984e170186367736
Kamil Korbik <korbik.gm@gmail.com>
2021-04-21 23:29:10 +0200

    Added first version

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,38 @@
 package main;
 
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.search.strategy.Search;
+import org.chocosolver.solver.variables.IntVar;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Działa i tyle koniec!");
+
+        int n = 8;
+
+        Model model = new Model("Problem " + n + " hetmanów");
+        IntVar[] queens = model.intVarArray("H", n, 1, n, false);
+        IntVar[] diagonal1 = new IntVar[n];
+        IntVar[] diagonal2 = new IntVar[n];
+
+        for(int i = 0 ; i < n; i++){
+            diagonal1[i] = queens[i].sub(i).intVar();
+            diagonal2[i] = queens[i].add(i).intVar();
+        }
+
+        model.post(
+                model.allDifferent(queens),
+                model.allDifferent(diagonal1),
+                model.allDifferent(diagonal2)
+        );
+
+        Solver solver = model.getSolver();
+        solver.setSearch(Search.domOverWDegSearch(queens));
+        Solution solution = solver.findSolution();
+
+        if (solution != null) {
+            System.out.println(solution.toString());
+        }
     }
 }
